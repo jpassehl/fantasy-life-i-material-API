11bab81c8723c4b14d8556209b4cb574d0bab5b7
jpassehl <JACQUELINEPASSEHL@gmail.com>
2025-09-19 00:27:10 -0500

    add Material Model

---
+++ Material.cs
@@ -0,0 +1,12 @@
+﻿namespace fantasy_life_i_material_API
+{
+    public class Material
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public bool Gatherable { get; set; }
+        public string[] GatheredFrom { get; set; }
+        public string LifeRequired { get; set; }
+        public string Category { get; set; }
+    }
+}
