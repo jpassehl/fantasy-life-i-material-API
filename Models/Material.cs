c121d8d6640b366c742de00e451194066834cd1a
jpassehl <JACQUELINEPASSEHL@gmail.com>
2025-09-26 18:42:16 -0500

    update GatheredFrom field to optional

--- Models/Material.cs
+++ Models/Material.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool Crafted { get; set; } = false;
-        public string[] GatheredFrom { get; set; }
+        public string?[] GatheredFrom { get; set; }
         public string? LifeRequired { get; set; }
         public string Category { get; set; }
     }
